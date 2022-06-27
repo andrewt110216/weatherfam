22fd61b46fcef509e8038ac649f3b37e365ee254
Andrew Tracey <89548595+andrewt110216@users.noreply.github.com>
2022-06-27 16:34:16 -0700

    reviewed urls.py

--- weather/urls.py
+++ weather/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('logout/', views.logout_request, name='logout'),
     path('registration/', views.registration_request, name='registration'),
     path('add-person/', views.add_person_request, name='add-person'),
+    path('update-person/<int:id>/', views.update_person, name='update-person'),
     path('detail-person/<int:id>/', views.detail_person, name='detail-person'),
     path('delete-person/<int:id>/', views.delete_person, name='delete-person'),
-    path('update-person/<int:id>/', views.update_person, name='update-person'),
 ]
