a18ca32ba02aa4fc3fbc0989be8d2f17eecd4e42
Andrew Tracey <89548595+andrewt110216@users.noreply.github.com>
2022-06-28 11:55:25 -0700

    bug fixes on views, reviewed html templates

--- weather/views.py
+++ weather/views.py
@@ -3,6 +3,7 @@ import requests
 import json
 from datetime import datetime, timedelta
 import pytz
+import time
 
 from django.shortcuts import  render, redirect
 from django.contrib.auth import login, logout, authenticate
@@ -14,8 +15,11 @@ from .models import Person, Weather, User_Person, Location
 from .funcs import get_codes, get_icon_path, get_timezone
 
 # Constants
+PRINT_DEBUG = True  # print statements for debugging
 BASE_DIR = Path(__file__).resolve().parent.parent
 API_RESULT_LIFETIME = timedelta(hours=6)
+# Track API requests per second to prevent exceeding limit of 3
+API_REQ_PER_SEC = {}  # minute-second: requests
 WEATHER_CODES, WEATHER_CODES_DAY = get_codes()
 CODES = {'hour': WEATHER_CODES, 'day': WEATHER_CODES_DAY}
 CODE_PARAM = {'hour': 'weatherCode', 'day': 'weatherCodeDay'}
@@ -28,9 +32,11 @@ def home(request):
     if request.user.is_authenticated:
         user = User.objects.get(username=request.user.username)
         user_persons = User_Person.objects.filter(user_id=user)
+        if PRINT_DEBUG: print(f"> User: {user.username}")
         for up in user_persons:
             location = up.person.location
             local_now = datetime.now(tz=pytz.timezone(location.timezone))
+            if PRINT_DEBUG: print(f"  - {up.person.name}, {location.name}")
             data = get_weather(location, local_now, 'hour')
             up.person.cur_weather = data
             local_dates = [local_now + timedelta(days=i) for i in range(3)]
@@ -121,6 +127,73 @@ def delete_person(request, id):
     return redirect("weather:home")
 
 # HELPER FUNCTIONS
+def get_weather(location: Location, local_now: datetime, period: str) -> dict:
+    data = {'temp': 'n/a', 'description': 'n/a', 'icon_path': None}
+    weather = query_database(location, local_now, period)
+    if weather is None or weather.timestamp + API_RESULT_LIFETIME < now():
+        if weather and weather.timestamp + API_RESULT_LIFETIME < now() and PRINT_DEBUG:
+            print(f"\t  - Database record expired: Weather Obj {weather.id}")
+        weather = api_request(weather, local_now, location, period)
+    if weather:
+        code = weather.weather_code
+        data['description'] = CODES[period][str(code)]
+        data['icon_path'] = get_icon_path(code)
+        data['temp'] = int(weather.temp)
+        data['day_name'] = local_now.strftime("%A")
+    return data
+
+def query_database(location: Location, local_now: datetime, period: str):
+    HOURS = {'day': 0, 'hour': local_now.time().hour}
+    if PRINT_DEBUG:
+        print(f"    - {local_now.date()} {period} {HOURS[period]}".ljust(24), end='')
+    try:
+        query = Weather.objects.filter(location=location, date=local_now.date(),
+                    hour=HOURS[period],step=TIMESTEPS[period])
+        weather = query.order_by("-timestamp")[:1].get()
+        if PRINT_DEBUG: print(f"  > Retrieved from database")
+        return weather
+    except Weather.DoesNotExist:
+        if PRINT_DEBUG: print(f"  > Not found in database")
+        return None
+
+def api_request(weather: Weather, current_time: datetime, location: Location,
+                period: str) -> Weather:
+    API_BASE_URL = 'https://api.tomorrow.io/v4/timelines'
+    API_KEY = 'P0upWa3NF5mlabyrkmlCDqZWDblfSUUc'
+    step = TIMESTEPS[period]
+    coordinates = str(location.latitude) + ',' + str(location.longitude)
+    params = {'location': coordinates,
+              'startTime': current_time.isoformat(),
+              'endTime': (current_time + TIMEDELTAS[period]).isoformat(),
+              'timesteps': step,
+              'fields': ['temperature', CODE_PARAM[period]],
+              'units': 'imperial',
+              'apikey': API_KEY,}
+    key = now().strftime('%M:%S')
+    API_REQ_PER_SEC[key] = API_REQ_PER_SEC.get(key, 0) + 1
+    if API_REQ_PER_SEC[key] > 3:
+        print('\t- Sleeping 1 second to avoid API request limit (3 per second)')
+        time.sleep(1)
+    response = requests.get(API_BASE_URL, params)
+    response_dict = response.json()
+    if response.status_code == 200:
+        intervals = response_dict['data']['timelines'][0]['intervals']
+        new_weathers = []
+        for interval in intervals:
+            new_weather = create_weather_obj(interval, location, period,
+                                             current_time, step)
+            new_weathers.append(new_weather)
+        if PRINT_DEBUG: print('\t> API request successful')
+        save_json_response(response_dict, new_weather)
+        return new_weathers[0]
+    elif response.status_code == 429:
+        if PRINT_DEBUG: print('\t> API request failed (429): limit reached')
+        if PRINT_DEBUG: print('\t  - Message', response_dict['message'])
+    elif response.status_code == 400:
+        if PRINT_DEBUG: print('\t> API request failed:', response_dict['type'])
+        if PRINT_DEBUG: print('\t  - Message:', response_dict['message'])
+    return weather
+
 def create_weather_obj(interval: list, location: Location, period: str,
                        current_time: datetime, step: str) -> Weather:
     time_utc = datetime.fromisoformat(interval['startTime'].replace('Z', ''))
@@ -141,61 +214,9 @@ def save_json_response(response: dict, weather: Weather) -> None:
     if out_directory.exists():
         with open(out_directory.joinpath(fn), 'w') as f:
             json.dump(response, f, indent=4)
-        print(' > Saved API response:', weather.location.name, file_time)
+        if PRINT_DEBUG: print('\t> Saved API response')
     else:
-        print(' > API response not saved. Outfile path does not exist.')
-
-def api_request(weather: Weather, current_time: datetime, location: Location,
-                period: str) -> Weather:
-    API_BASE_URL = 'https://api.tomorrow.io/v4/timelines'
-    API_KEY = 'P0upWa3NF5mlabyrkmlCDqZWDblfSUUc'
-    step = TIMESTEPS[period]
-    coordinates = str(location.latitude) + ',' + str(location.longitude)
-    params = {'location': coordinates,
-              'startTime': current_time.isoformat(),
-              'endTime': (current_time + TIMEDELTAS[period]).isoformat(),
-              'timesteps': step,
-              'fields': ['temperature', CODE_PARAM[period]],
-              'units': 'imperial',
-              'apikey': API_KEY,}
-    response = requests.get(API_BASE_URL, params)
-    response_dict = response.json()
-    if response.status_code == 200:
-        intervals = response_dict['data']['timelines'][0]['intervals']
-        new_weathers = []
-        for interval in intervals:
-            new_weather = create_weather_obj(interval)
-            new_weathers.append(new_weather)
-            save_json_response(response_dict, new_weather)
-        return new_weathers[0]
-    elif response.status_code == 429:
-        print('> Log: API request limit has been reached!')
-    elif response.status_code == 400:
-        print('> Log: API request failed due to:', response_dict['type'])
-        print('\t> Response Message:', response_dict['message'])
-    return weather
-
-def query_database(location: Location, local_now: datetime, period: str):
-    HOURS = {'day': 0, 'hour': local_now.time().hour}
-    try:
-        query = Weather.objects.filter(location=location, date=local_now.date(),
-                    hour=HOURS[period],step=TIMESTEPS[period])
-        return query.order_by("-timestamp")[:1].get()
-    except Weather.DoesNotExist:
-        return None
-
-def get_weather(location: Location, local_now: datetime, period: str) -> dict:
-    data = {'temp': 'n/a', 'description': 'n/a', 'icon_path': None}
-    weather = query_database(location, local_now, period)
-    if weather is None or weather.timestamp + API_RESULT_LIFETIME < now():
-        weather = api_request(weather, local_now, location, period)
-    if weather:
-        code = weather.weather_code
-        data['description'] = CODES[period][str(code)]
-        data['icon_path'] = get_icon_path(code)
-        data['temp'] = int(weather.temp)
-        data['day_name'] = local_now.strftime("%A")
-    return data
+        if PRINT_DEBUG: print("\t> API response not saved: path doesn't exist)")
 
 def get_location(lat: str, long: str, request) -> Location:
     try:
