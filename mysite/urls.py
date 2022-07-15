828f477df9d81bcf68e08d78f9a3d1e251cbecf5
Andrew Tracey <89548595+andrewt110216@users.noreply.github.com>
2022-07-14 21:43:59 -0700

    update urls to remote weather/ from url so landing page works

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('weather/', include('weather.urls')),
+    path('', include('weather.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
