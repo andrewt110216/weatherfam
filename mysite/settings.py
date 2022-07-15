1ba43f11e1ba95a0bc7824332724618d38d60a1a
Andrew Tracey <89548595+andrewt110216@users.noreply.github.com>
2022-07-14 22:09:31 -0700

    move additional db settings to config

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'HOST': '127.0.0.1',
+        'HOST': os.environ.get("MYSQL_HOST"), # '127.0.0.1',
         'PORT': os.environ.get("MYSQL_PORT"),
         'USER': os.environ.get("MYSQL_USER_NAME"),
         'PASSWORD': os.environ.get("MYSQL_PASSWORD"),
-        'NAME': 'beta_db',
+        'NAME': os.environ.get("MYSQL_DB_NAME"),
     }
 }
 
