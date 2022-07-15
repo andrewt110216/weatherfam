e38dccb89d705578d645980eeff3af887d752901
Andrew Tracey <89548595+andrewt110216@users.noreply.github.com>
2022-07-14 21:35:46 -0700

    adjust allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get("DEBUG") == "True")
 
-ALLOWED_HOSTS = ['https://weatherfam.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['weatherfam.herokuapp.com','127.0.0.1']
 
 
 # Application definition
